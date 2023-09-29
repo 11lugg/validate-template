530f468e0b9dc541d9e55c866e6f6d58bfb39802
11lugg <jlugg89@gmail.com>
2023-09-29 16:52:13 +0100

    form validation

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -4,42 +4,68 @@ import jsonp from "jsonp";
 
 function SignupForm() {
   const [email, setEmail] = useState("");
+  const [result, setResult] = useState({ error: "", success: "" });
+  const [placeholder, setPlaceholder] = useState("Enter email");
 
   const onSubmit = (e) => {
     e.preventDefault();
+    setResult({ error: "", success: "" });
+
     const url = process.env.REACT_APP_MAILCHIMP_URL;
 
     const modifiedUrl = url.replace("/post?", "/post-json?");
 
-    jsonp(`${modifiedUrl}&EMAIL=${email}`, { param: "c" }, (_, data) => {
-      const { msg } = data;
-      alert(msg);
+    jsonp(`${modifiedUrl}&EMAIL=${email}`, { param: "c" }, (error, data) => {
+      if (error) {
+        setResult({ ...result, error: error });
+
+        console.error(`Error while fetching data: ${error}`);
+        return;
+      }
+      setResult({ ...result, success: data });
+      setEmail("");
+      setPlaceholder("All done!");
     });
   };
 
-  return (
-    <Form onSubmit={onSubmit}>
-      <Row>
-        <Col lg={8} className="mb-3 mb-lg-0">
-          <Form.Group controlId="formBasicEmail">
-            <Form.Control
-              size="lg"
-              type="email"
-              name="EMAIL"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter email"
-            />
-          </Form.Group>
-        </Col>
+  const onChange = (e) => {
+    setResult({ error: "", success: "" });
+    setPlaceholder("Enter email");
 
-        <Col lg={4}>
-          <Button className="btn-lg" style={{ width: "100%" }} type="submit">
-            Submit
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+    setEmail(e.target.value);
+  };
+
+  return (
+    <>
+      <Form onSubmit={onSubmit}>
+        <Row>
+          <Col lg={8} className="mb-3 mb-lg-0">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control
+                size="lg"
+                type="email"
+                name="EMAIL"
+                className={
+                  result.success ? "is-valid" : result.error ? "is-invalid" : ""
+                }
+                required
+                onChange={onChange}
+                placeholder={placeholder}
+                value={email}
+              />
+            </Form.Group>
+          </Col>
+
+          <Col lg={4}>
+            <Button className="btn-lg" style={{ width: "100%" }} type="submit">
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+      {result.error && <p className="text-danger">{result.error}</p>}
+      {result.success && <p className="text-success">{result.success.msg}</p>}
+    </>
   );
 }
 
